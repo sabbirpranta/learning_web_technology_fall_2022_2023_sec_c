1c88e36bb1477cc7f7c304a6913d63f29eadf645
sabbirpranta <blackcat.mi.34955@gmail.com>
2022-10-16 16:12:25 +0600

    lab_3_php_submit

---
+++ namecheck.php
@@ -0,0 +1,14 @@
+<?php 
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    if($username == "" || $password == ""){
+        echo "invalid username/password";
+    }else if($username == $password){
+        echo "valid user";
+    }else{
+        echo "invalid user!";
+    }
+
+?>
